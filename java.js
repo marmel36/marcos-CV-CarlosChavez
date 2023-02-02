8c64383da23f6edb92450e7d437037829e067651
marmel36 <marcosmelilli30@gmail.com>
2023-02-01 21:34:04 -0300

    se agrega el proyecto

---
+++ java.js
@@ -0,0 +1,17 @@
+document.getElementById("maps").onclick = function (){
+
+    document.location.href = "https://www.google.com/maps/?hl=es" ;
+
+}
+
+document.getElementById("email").onclick = function (){
+
+    document.location.href = "https://www.google.com/intl/es-419/gmail/about/" ;
+
+}
+
+document.getElementById("redes").onclick = function (){
+
+    document.location.href = "https://www.instagram.com/" ;
+
+}
